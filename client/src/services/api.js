985b8e0353d86fdba9e65cc267452218c282bc36
tanvi <tanvi@gmail.com>
2026-07-04 10:42:13 +0530

    Resolve merge conflict and fix API config

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  
-baseURL: process.env.REACT_APP_API_URL + "/api",
+  baseURL: process.env.REACT_APP_API_URL + "/api",
 });
 
 // Automatically attach JWT token
